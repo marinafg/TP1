615b0f59409b4f6170829dfb9bc552d2e969fd2f
Marina <guimaraes.marina@hotmail.com>
2019-04-26 20:55:47 -0300

    correcoes

--- Matriz.cpp
+++ Matriz.cpp
@@ -6,22 +6,44 @@ using namespace std;
 Matriz::Matriz(int numeroLinhas, int numeroColunas, const double &valor){
 	_numeroColunas = numeroColunas;
 	_numeroLinhas = numeroLinhas;
-	_valor = valor;
+	val = new double*[numeroLinhas];
+	for (int i=0; i<numeroLinhas; i++){
+		val[i] = new double[numeroColunas];
+		for (int j = 0; j<numeroColunas; j++){
+			val[i][j] = valor;
+		}
+	}
 }
 
 Matriz::Matriz()
 {
 	_numeroColunas = 0;
 	_numeroLinhas = 0;
+	val = NULL;
 }
 
-Matriz::~Matriz()
+Matriz::~Matriz()  // destrutor
 {
+	for (int i = 0; i<_numeroLinhas; i++){
+		delete[] val[i];
+	}
+	delete[] val;
 }
 
-Matriz::Matriz(const Matriz &m){ //construtor de copia
+Matriz::Matriz(const Matriz &m){    //construtor de copia
     _numeroColunas = m._numeroColunas;
     _numeroLinhas = m._numeroLinhas;
+    if (m.val){
+    	val = new double*[_numeroLinhas];
+    	for (int i = 0; i<_numeroLinhas; i++){
+    		val[i] = new double[_numeroColunas];
+		}
+		for (int i = 0; i<_numeroLinhas; i++){
+			for(int j = 0; j<_numeroColunas; j++){
+				val[i][j] = m.val[i][j];
+			}
+		}
+	}
 }
 
 void Matriz::setCols(){
@@ -32,14 +54,39 @@ void Matriz::setRows(){
 	cin >> _numeroLinhas;
 }
 
-void Matriz::eye(int Linha, int Coluna){
-	for (Linha=0; Linha < _numeroLinhas; Linha++){
-		for (Coluna=0; Coluna < _numeroColunas; Coluna++){
-			if (Linha == Coluna){
-				int _valor = 1;
+int Matriz::getCols() const {
+	return _numeroLinhas;
+}
+
+int Matriz::getRows() const {
+	return _numeroColunas;
+}
+
+void Matriz::eye(int Linha, int Coluna){  //cria matriz identidade
+	for (int i = 0; i < _numeroLinhas; i++){
+		for (int j = 0; j < _numeroColunas; j++){
+			if (i == j){
+				val[i][j] = 1;
 			}else{
-				int _valor = 0;
+				val[i][j] = 0;
 			}
 		}
+	}
+}
+
+void Matriz::zeros(){
+  for(int i=0; i<_numeroLinhas; i++){
+    for(int j=0; j<_numeroColunas; j++)
+      val[i][j] = 0;
+    }
+}
+
+Matriz Matriz::operator+(const Matriz& a) const{
+	if (_numeroLinhas != a._numeroLinhas || _numeroColunas != a._numeroColunas){
+		cout << "operacao invalida. matrizes de dimencoes diferentes." << endl;
+	}
+	else {
+		Matriz resp(a._numeroLinhas, a._numeroColunas);
+		
 	}
 }
--- Matriz.h
+++ Matriz.h
@@ -8,24 +8,26 @@ class Matriz
 	private:
 		int _numeroLinhas;
 		int _numeroColunas;
-		double _valor;
+		double **val;
 		
 	public:
 		Matriz(); // cria uma matriz vazia com números de linhas e colunas iguais a 0
-		Matriz(int = 0, int = 0, const double &valor = 0); /*cria uma matriz com 
+		Matriz(int, int, const double &valor = 0); /*cria uma matriz com 
 												  			número de linhas e colunas iguais aos recebidos pelos argumentos e com todos
 												  			os valores da matriz preenchidos com a variável valor.*/
 		Matriz(const Matriz &m);  //construtor de cópia
 		~Matriz(); // destrutor 
 		void setCols();
 		void setRows();
-		int getRows();
-		int getCols();
+		int getRows() const;
+		int getCols() const;
 		void eye(int, int); // cria matriz identidade
 		float det(); // calcula o determinante
 		float trace(); // calcula o traco
+		void zeros(); // cria matriz com tds elementos iguais a 0
 		
-	protected:
+		Matriz operator+(const Matriz&) const;
+		Matriz operator*(const Matriz&) const;
 };
 
 #endif
