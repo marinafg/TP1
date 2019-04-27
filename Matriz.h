c2824be1a8729ae929fd64aaec2d3acfef179656
ianasten <38948409+ianasten@users.noreply.github.com>
2019-04-26 22:15:52 -0300

    update .h

--- Matriz.h
+++ Matriz.h
@@ -15,7 +15,7 @@ class Matriz
 		Matriz(int, int, const double &valor = 0); /*cria uma matriz com
 												  			número de linhas e colunas iguais aos recebidos pelos argumentos e com todos
 												  			os valores da matriz preenchidos com a variável valor.*/
-		Matriz(const Matriz &m);  //construtor de cópia
+		Matriz(const Matriz &);  //construtor de cópia
 		~Matriz(); // destrutor
 		void setCols();
 		void setRows();
@@ -30,16 +30,16 @@ class Matriz
 		Matriz operator*(const Matriz&) const;
 		Matriz operator-(const Matriz&) const;
 		Matriz operator+=(const Matriz&);
-		Matriz operator=(const Matriz& a);
-		bool operator!=(const Matriz& a) const;
-		bool operator==(const Matriz& a) const;
+		Matriz operator=(const Matriz&);
+		bool operator!=(const Matriz&) const;
+		bool operator==(const Matriz&) const;
 		friend istream& operator >> (istream&, Matriz&);
 		friend ostream& operator << (ostream&, const Matriz&);
-		Matriz& operator-=(const Matriz& a);
-		Matriz& operator*=(const Matriz& a);
+		Matriz& operator-=(const Matriz&);
+		Matriz& operator*=(const Matriz&);
 		Matriz operator~();
-		Matriz& operator*=(double valor);
-		double& operator()(int numeroLinhas, int numeroColunas);
+		Matriz& operator*=(double);
+		double& operator()(int, int);
 };
 
 #endif
