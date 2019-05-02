3d0b40ff72f6ee2e23b1f6d321b64be68014d6b2
Marina <guimaraes.marina@hotmail.com>
2019-05-02 18:14:07 -0300

    update novo

--- main.cpp
+++ main.cpp
@@ -5,8 +5,10 @@ using namespace std;
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main() {
-	Matriz A(3,4);
-	A.printMatriz();
+	Matriz A(3,4,2);
+	Matriz B(3,4,3);
+	Matriz C;
+	C = A + B;
+	C.printMatriz();
 	return 0;
-	
 }
